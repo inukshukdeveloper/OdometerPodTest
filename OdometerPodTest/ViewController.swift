ba986fb3ad865fc9f19c140a09afde5543c49c0e
Inukshuk Software, LLC <mark@inuksoft.com>
2019-10-23 10:36:02 -0700

    Made OdometerView use proper pods module.

--- OdometerPodTest/ViewController.swift
+++ OdometerPodTest/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import Odometer
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var odometerView: OdometerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        odometerView.updateMileage(mileage: 1234, spin: true, maxSpinTime: 5.0)
+        let oView = OdometerView(frame: CGRect(x: 20.0, y: 200.0, width: 160.0, height: 50.0), digitNum: 4, fontsize: 35)
+        self.view.addSubview(oView)
+        oView.updateMileage(mileage: 6411, spin: true, maxSpinTime: 10.0)
     }
-
-
 }
 
